30f5d587934ace46dbdbea81fb505853b36bac3a
ryo-ta-no <kabhi4w@gmail.com>
2019-09-10 20:02:34 +0900

    変更

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -14,17 +14,9 @@
     <!-- リスト表示部分 -->
     <div>
       <ul class="collection">
-        <li id="row_task_1" class="collection-item">
-          <input type="checkbox" id="task_1" />
-          <label for="task_1">Sample Task</label>
-        </li>
-        <li id="row_task_2" class="collection-item">
-          <input type="checkbox" id="task_2" />
-          <label for="task_2">Sample Task</label>
-        </li>
-        <li id="row_task_3" class="collection-item">
-          <input type="checkbox" id="task_3" />
-          <label for="task_3">Sample Task</label>
+        <li v-for="task in tasks" v-if="!task.is_done" v-bind:id="'row_task_' + task.id" class="collection-item">
+          <input type="checkbox" v-bind:id="'task_' + task.id" />
+          <label v-bind:for="'task_' + task.id">{{ task.name }}</label>
         </li>
       </ul>
     </div>
@@ -33,15 +25,38 @@
     <!-- 完了済みタスク一覧 -->
     <div id="finished-tasks" class="display_none">
       <ul class="collection">
-        <li id="row_task_4" class="collection-item">
-          <input type="checkbox" id="'task_4" checked="checked" />
-          <label v-bind:for="task_4" class="line-through">Done Task</label>
-        </li>
-        <li id="row_task_5" class="collection-item">
-          <input type="checkbox" id="'task_5" checked="checked" />
-          <label v-bind:for="task_5" class="line-through">Done Task</label>
+        <li v-for="task in tasks" v-if="task.is_done"v-bind:id="'row_task_' + task.id" class="collection-item">
+          <input type="checkbox" v-bind:id="'task_' + task.id" checked="checked" />
+          <label v-bind:for="'task_' + task.id"  class="line-through">{{ task.name }}</label>
         </li>
       </ul>
     </div>
   </div>
 </template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data: function () {
+      return {
+        tasks: [],
+        newTask: ''
+      }
+    },
+    mounted: function () {
+      this.fetchTasks();
+    },
+    methods: {
+      fetchTasks: function () {
+        axios.get('/api/tasks').then((response) => {
+          for(var i = 0; i < response.data.tasks.length; i++) {
+            this.tasks.push(response.data.tasks[i]);
+          }
+        }, (error) => {
+          console.log(error);
+        });
+      },
+    }
+  }
+</script>
